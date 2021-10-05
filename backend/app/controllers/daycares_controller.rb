cb9c0ee49502046561d61b559bb3ae6c417e9c3b
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-10-05 15:42:07 -0500

    updates

--- backend/app/controllers/daycares_controller.rb
+++ backend/app/controllers/daycares_controller.rb
@@ -5,12 +5,12 @@ class DaycaresController < ApplicationController
   def index
     @daycares = Daycare.all
 
-    render json: @daycares
+    render json: @daycares, except: [:created_at, :updated_at], include: [:children]
   end
 
   # GET /daycares/1
   def show
-    render json: @daycare
+    render json: @daycare, except: [:created_at, :updated_at], include: [:children]
   end
 
   private
