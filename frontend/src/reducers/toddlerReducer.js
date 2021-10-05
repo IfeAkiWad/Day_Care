26b7aa189d0fb930501475db95438cb1573214a0
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-10-05 16:42:27 -0500

    update reducer

--- frontend/src/reducers/toddlerReducer.js
+++ frontend/src/reducers/toddlerReducer.js
@@ -5,14 +5,13 @@ const toddlerReducer = (state = {toddlers: []}, action) => {
             ...state, toddlers: [...state.toddlers], loading: true
         }
         case 'ADD_TODDLER':
-            // get rid of the toddler that exists and add a new one ???
             return {
                 ...state,
                 toddlers: action.toddlers,
                 loading: false
             }
             // return {...state, toddlers: [...state.toddlers, action.toddler]} 
-        case "REMOVE_toddler":
+        case "REMOVE_TODDLER":
             const newtoddlers = state.toddlers.filter(toddler => toddler.id !== action.id)
             return {...state, toddlers: newtoddlers}
             default:
