f0894160c888634a811489a9f5ba325b099d7623
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-11-26 19:11:13 -0600

    added like buttons in image gallery

--- frontend/src/components/ImageGallery.js
+++ frontend/src/components/ImageGallery.js
@@ -6,6 +6,19 @@ import ToddlerFriends from '../ToddlerFriends.jpeg'
 
 
 class ImageGallery extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+
+    likeIncrement = () => {
+        let newCount = this.state.count + 1
+        this.setState({count: newCount})
+    }
+
     render() {
         return (
             <div id='image'>
@@ -13,19 +26,23 @@ class ImageGallery extends Component {
                 <div id='image-div'>
                     <h2 id='image-htwo'>We believe learning comes from interactive play!</h2>
                 </div>
-                <img className="toddler-image" src={Image} alt='Toddlers playing'/>
+                <img className="toddler-image" src={Image} alt='Toddlers playing'/><br />
+                <button id='likes' onClick={this.likeIncrement}> ❤️ {this.state.count}</button>
                 <div>
                     <h2 id='image-htwo'>We believe words are fun!</h2>
                 </div>
-                <img className="toddler-image" src={ToddlerRead} alt='Toddler-learning' />
+                <img className="toddler-image" src={ToddlerRead} alt='Toddler-learning' /><br />
+                <button id='likes' onClick={this.likeIncrement}> ❤️ {this.state.count}</button>
                 <div>
                     <h2 id='image-htwo'>We believe in moving our bodies!</h2>
                 </div>
-                <img className="toddler-image" src={ToddlersMoving} alt='Toddler-learning' />
+                <img className="toddler-image" src={ToddlersMoving} alt='Toddler-learning' /><br />
+                <button id='likes' onClick={this.likeIncrement}> ❤️ {this.state.count}</button>
                 <div>
                     <h2 id='image-htwo'>We believe in friendship!</h2>
                 </div>
-                <img className="toddler-image" src={ToddlerFriends} alt='Toddler-learning' />
+                <img className="toddler-image" src={ToddlerFriends} alt='Toddler-learning' /><br />
+                <button id='likes' onClick={this.likeIncrement}> ❤️ {this.state.count}</button>
     
     
             </div>
