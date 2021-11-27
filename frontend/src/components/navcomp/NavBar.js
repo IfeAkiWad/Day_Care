13cbfe13fcaa0efb197aba118228c32e8785213c
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-11-26 18:14:06 -0600

    removed flower emojis in NavBar.js

--- frontend/src/components/navcomp/NavBar.js
+++ frontend/src/components/navcomp/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
     return (
         <nav id="nav">
         <div>
-             🌻 <Link id="link" to="/">HOME</Link> 🌻 
-                <Link id="link" to="/daycares/toddlers/new"> NEW STUDENT</Link> 🌻
-                <Link id="link" to="/toddlers"> TODDLERS</Link> 🌻 
-                <Link id="link" to='/gallery'> PHOTO GALLERY</Link> 🌻
+                <Link id="link" to="/">HOME</Link>  
+                <Link id="link" to="/daycares/toddlers/new"> NEW STUDENT</Link> 
+                <Link id="link" to="/toddlers"> TODDLERS</Link> 
+                <Link id="link" to='/gallery'> PHOTO GALLERY</Link> 
         </div>
     </nav>
     )
