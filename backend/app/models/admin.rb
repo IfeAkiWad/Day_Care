eac3079bc0754b388e22fe97619961b3585fb397
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-11-27 22:57:31 -0600

    added has_secure_password and validation to admin

--- backend/app/models/admin.rb
+++ backend/app/models/admin.rb
@@ -1,2 +1,4 @@
 class Admin < ApplicationRecord
+    has_secure_password #used to encrypt and authenticate passwords using BCrypt
+    validates :username, uniqueness: { case_sensitive: false }
 end
