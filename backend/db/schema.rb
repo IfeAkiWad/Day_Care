6f17bac04eddd08144cc08222050c1e774453fe7
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-11-27 23:33:20 -0600

    migrated admins table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_221546) do
+ActiveRecord::Schema.define(version: 2021_11_28_041008) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "daycares", force: :cascade do |t|
     t.string "name"
