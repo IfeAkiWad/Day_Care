f8ec08bd39cc9de0c0ab7372e23f0b9b447a56ca
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-11-28 19:47:24 -0600

    created routes for authentication

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'admins#new'
+  post '/signup', to: 'admins#create'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :admins
   resources :toddlers
 
