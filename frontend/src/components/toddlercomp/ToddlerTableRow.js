07fd4b6d9726cfc784583c688c47723e3628fb54
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-12 19:35:34 -0600

    creating link to form

--- frontend/src/components/toddlercomp/ToddlerTableRow.js
+++ frontend/src/components/toddlercomp/ToddlerTableRow.js
@@ -22,6 +22,7 @@ export default class ToddlerTableRow extends Component {
                             </tr>
                         </tbody>
                 </table>
+            
             </div>
         )
     }
--- frontend/src/containers/ToddlerContainer.js
+++ frontend/src/containers/ToddlerContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ToddlerFilterContainer from './ToddlerFilterContainer'
 import ToddlerTableRow from '../components/toddlercomp/ToddlerTableRow'
+import {Link} from 'react-router-dom'
+
 
 // Daycare component mapping through the props recieved from daycare container and passing props down to toddlerlist and toddler conatainer
 
@@ -14,6 +16,7 @@ class ToddlerContainer extends Component {
                     {this.props.getDaycare.toddlers.map( toddler => { //Passing props containing an array of toddlers and  their info
                         return <ToddlerTableRow key={toddler.id} toddlerDetail={toddler} />
                     })}
+                    <Link to="/daycares/toddlers/new">Register Another Student</Link>
                      {/* passing props of the whole daycare object */}
                 <ToddlerFilterContainer getToddlers={this.props.getDaycare}/> 
             </div>
