bec57530c42e64d1629124bfdad8c4d414dae8f2
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-16 22:47:32 -0600

    seeding db for Admin

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,15 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Daycare.create(name: 'Sunny Day Care', address: '100 W 113th St. New York, NY 10026')
 
-# Daycare.create(name: 'Tator Totts Academy', address: '1999 Clark St. Victorville, CA 92392')
+Admin.create(username: '#1098', password: 'jxmxe')
+Admin.create(username: '#9987', password: 'honxe')
+# Admin.create(username: '#6543', password: 'moxdeyx')
+# Admin.create(username: '#2150', password: 'loxelyx')
 
 
-Toddler.create(name: 'Mario Lopez', birthday: '2018-11-03', contact: 'Leslie Lopez', phone: 2125557894, allergy: 'none', daycare_id: 1)
+# Daycare.create(name: 'Sunny Day Care', address: '100 W 113th St. New York, NY 10026')
 
-Toddler.create(name: 'Raynese Potter', birthday: '2019-05-12', contact: 'Mark Potter', phone: 7185553256, allergy: 'peanut', daycare_id: 1)
+# # Daycare.create(name: 'Tator Totts Academy', address: '1999 Clark St. Victorville, CA 92392')
 
-Toddler.create(name: 'Tony Giacomo', birthday: '2018-02-03', contact: 'Maria Gandolfini', phone: 2125551469, allergy: 'none', daycare_id: 1)
 
-Toddler.create(name: 'Cynthia Moreno', birthday: '2019-04-19', contact: 'Carla De Miguel', phone: 2125553369, allergy: 'none', daycare_id: 1)
+# Toddler.create(name: 'Mario Lopez', birthday: '2018-11-03', contact: 'Leslie Lopez', phone: 2125557894, allergy: 'none', daycare_id: 1)
+
+# Toddler.create(name: 'Raynese Potter', birthday: '2019-05-12', contact: 'Mark Potter', phone: 7185553256, allergy: 'peanut', daycare_id: 1)
+
+# Toddler.create(name: 'Tony Giacomo', birthday: '2018-02-03', contact: 'Maria Gandolfini', phone: 2125551469, allergy: 'none', daycare_id: 1)
+
+# Toddler.create(name: 'Cynthia Moreno', birthday: '2019-04-19', contact: 'Carla De Miguel', phone: 2125553369, allergy: 'none', daycare_id: 1)
