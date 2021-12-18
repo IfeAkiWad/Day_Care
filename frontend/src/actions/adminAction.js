0a9e87ed876f8e6fba3f2aa47cc7319d03c60b3b
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-17 22:55:34 -0600

    built admin action

--- frontend/src/actions/adminAction.js
+++ frontend/src/actions/adminAction.js
@@ -4,7 +4,7 @@ export const fetchAdmins = () => {
       fetch("http://localhost:3000/admins")
         .then(response => response.json())
         .then((data) => {
-          dispatch({ type: "ADD_ADMINS", admins: data });
+          dispatch({ type: "LOGIN_ADMINS", admins: data });
         });
     };
   };
